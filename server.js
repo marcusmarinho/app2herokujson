09b6a12e13492d3064bb68e2a2e5d5ddadf3f68b
marcusmarinho <marcus.marinho@gmail.com>
2019-10-26 19:17:02 -0300

    changes

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/personalproject'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/personalproject/index.html'));
+});
+
+// default Heroku port
+
+app.listen(process.env.PORT || 8080, function(){
+  console.log('Servidor On')
+});
