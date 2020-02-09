df60a30eeb57057175d787c1df61bf7a0574bee3
marcusmarinho <marcus.marinho@gmail.com>
2020-02-08 21:50:05 -0300

    fix

--- src/app/shared/components/spinner-interceptor.service.ts
+++ src/app/shared/components/spinner-interceptor.service.ts
@@ -12,7 +12,6 @@ export class SpinnerInterceptorService implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.spinnerService.show();
-    debugger;
     return next.handle(request).pipe(
       tap((event: HttpEvent<any>) => {
         if (event instanceof HttpResponse) {
