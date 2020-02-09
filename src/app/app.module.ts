53d460c8289fcd19dc36a3aa2ff83b4486072a50
marcusmarinho <marcus.marinho@gmail.com>
2020-02-08 21:43:20 -0300

    imported spinner and material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 // LOCALE ID pacote intl
-import { NgModule, LOCALE_ID} from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
 registerLocaleData(ptBr);
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // Component
 import { AppComponent } from './app.component';
 
@@ -22,18 +22,14 @@ import { SearchService } from '../app/shared/services/search.service';
 import { CrudService } from '../app/shared/services/crud.service';
 
 // Angular Material
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
-
-
+import { SpinnerInterceptorService } from './shared/components/spinner-interceptor.service';
+import { MaterialModule } from './shared/modules/material.module';
+import { SpinnerModule } from './shared/components/spinner.module';
 
 @NgModule({
-    declarations: [
+  declarations: [
     AppComponent,
     HomeComponent,
     DescricaoReduzida,
@@ -44,16 +40,19 @@ import {MatSelectModule} from '@angular/material/select';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MatCardModule,
-    MatButtonModule,
-    MatMenuModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    MatSelectModule,
+    MaterialModule,
+    SpinnerModule
   ],
-  providers: [ {  provide: LOCALE_ID, useValue: 'pt'},
-                CartService, OfferService, OrderPurchaseService, SearchService, CrudService
-             ],
+  providers: [
+    {
+    provide: HTTP_INTERCEPTORS,
+    useClass: SpinnerInterceptorService,
+    multi: true
+  },
+  { provide: LOCALE_ID, useValue: 'pt' },
+    CartService, OfferService, OrderPurchaseService, SearchService, CrudService, SpinnerInterceptorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
